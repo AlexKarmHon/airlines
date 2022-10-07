9f21bc72f548c89a9dac58aa59bf56e2ba524ba9
unknown <AlexKarmHon>
2022-10-07 20:38:37 +0300

    add

---
+++ users/views.py
@@ -0,0 +1,41 @@
+from audioop import reverse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
+
+
+# Create your views here
+def index(request):
+    #Якщо користувач не увійшов до системи, повернутись на сторінку входу
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    return render(request, 'users/user.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        #Отримуємо доступ до ім'я користувача та пароля з даних
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #Перевіряємо, чи ім'я та пароль правильні, повертаємо об'єкт User, якщо все првильно
+        user = authenticate(request, username=username, password=password)
+
+        #Якщо об'єкт user повернуто, увійти до системи та повернутися на головну сторінку
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        #В іншому випадку, повернутись до сторінки входу з новим контекстом
+        else:
+            return render(request, 'users/login.html', {
+                'message': "Invalid Credentials"
+            })
+
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/login.html', {
+        'message': "Logged Out"
+    })
